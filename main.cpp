18399ecacd1f64ede3e025c02d8ac951f6cf7f4b
US-S-R <59696194+US-S-R@users.noreply.github.com>
2020-08-13 17:48:02 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<iomanip>
+#include<utility>
+#include <algorithm>
+
+long double sine(long double angle){
+
+
+return  ;
+}
+
+
+    int main(){
+    
+    
+    
+    }
+
